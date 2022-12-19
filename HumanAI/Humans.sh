666fc5c5ced4584d5e6614fca58211af1989d333
vinjan23 <108977419+vinjan23@users.noreply.github.com>
2022-12-19 12:34:50 +0700

    Update Humans.sh

--- HumanAI/Humans.sh
+++ HumanAI/Humans.sh
@@ -36,7 +36,7 @@ sudo apt-get install make build-essential gcc git jq chrony screen -y
 
 # install go
 if ! [ -x "$(command -v go)" ]; then
-  ver="1.18.2"
+  ver="1.19"
   cd $HOME
   wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
   sudo rm -rf /usr/local/go
