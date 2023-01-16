b00808461ca7f88dc8bde3908b9a7f81804153e9
vinjan <108977419+vinjan23@users.noreply.github.com>
2023-01-16 11:33:25 +0700

    Update auto.sh

--- DeFund/Private-4/auto.sh
+++ DeFund/Private-4/auto.sh
@@ -27,6 +27,9 @@ sleep 2
 echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
 # update
 sudo apt -q update
+
+echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
+# packages
 sudo apt -qy install curl git jq lz4 build-essential
 sudo apt -qy upgrade
 
@@ -45,14 +48,7 @@ fi
 echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
 # download and build binaries
 cd $HOME
-rm -rf defundver="1.19.1"
-cd $HOME
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
-rm "go$ver.linux-amd64.tar.gz"
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
-source $HOME/.bash_profile
+rm -rf defund
 git clone https://github.com/defund-labs/defund.git
 cd defund
 git checkout v0.2.2
