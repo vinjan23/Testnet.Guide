ee9925999217095c2dbc012517ed2e7151e4beda
vinjan <108977419+vinjan23@users.noreply.github.com>
2023-01-17 12:31:24 +0700

    Update snapshot.sh

--- HumanAI/snapshot.sh
+++ HumanAI/snapshot.sh
@@ -1,12 +1,16 @@
+# install dependencies, if needed
 sudo apt update
-sudo apt install snapd -y
-sudo snap install lz4
+sudo apt install lz4 -y
 
 sudo systemctl stop humansd
 
+cp $HOME/.humans/data/priv_validator_state.json $HOME/.humans/priv_validator_state.json.backup
 humansd tendermint unsafe-reset-all --home $HOME/.humans --keep-addr-book
 
-curl -L https://snap.nodeist.net/t/humans/humans.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.humans --strip-components 2
+SNAP_NAME=$(curl -s https://snapshots4-testnet.nodejumper.io/humans-testnet/ | egrep -o ">testnet-1.*\.tar.lz4" | tr -d ">")
+curl https://snapshots4-testnet.nodejumper.io/humans-testnet/${SNAP_NAME} | lz4 -dc - | tar -xf - -C $HOME/.humans
 
-sudo systemctl start humansd && journalctl -u humansd -f --no-hostname -o cat
+mv $HOME/.humans/priv_validator_state.json.backup $HOME/.humans/data/priv_validator_state.json
 
+sudo systemctl restart humansd
+sudo journalctl -u humansd -f -o cat
