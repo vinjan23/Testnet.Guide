b777f70083f98eb23aec1dc608eb68b85a9b0daa
vinjan <108977419+vinjan23@users.noreply.github.com>
2023-02-09 21:10:34 +0700

    Update auto.sh

--- Coreum/auto.sh
+++ Coreum/auto.sh
@@ -8,7 +8,7 @@ echo "      #     #     #  #     #  #          #    #  # # #    #     #  #    ";
 echo "       #   #      #  #      # #          #   #        #   #      # #    ";
 echo "         #        #  #        #   # # # #   #          #  #        #    ";
 echo -e "\e[0m"
-sleep 1
+sleep 2
 
 read -p "Enter node moniker: " MONIKER
 
@@ -22,7 +22,31 @@ export CORE_CHAIN_ID_ARGS="--chain-id=$CORE_CHAIN_ID"
 export CORE_NODE_ARGS="--node=$CORE_NODE $CORE_CHAIN_ID_ARGS"
 export CORE_HOME=$HOME/.core/"$CORE_CHAIN_ID"
 export CORE_BINARY_NAME=$(arch | sed s/aarch64/cored-linux-arm64/ | sed s/x86_64/cored-linux-amd64/)
+sleep 2
 
+echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
+# update
+sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
+# packages
+sudo apt install curl tar wget clang pkg-config libssl-dev libleveldb-dev jq build-essential bsdmainutils git make ncdu htop screen unzip bc fail2ban htop -y
+
+# install go
+if ! [ -x "$(command -v go)" ]; then
+ver="1.19.3"
+cd $HOME
+rm -rf go
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+source ~/.bash_profile
+fi
+
+echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
+# download and build binaries
 mkdir -p $CORE_HOME/bin
 curl -LO https://github.com/CoreumFoundation/coreum/releases/download/$CORE_VERSION/$CORE_BINARY_NAME
 mv $CORE_BINARY_NAME $CORE_HOME/bin/cored
@@ -30,5 +54,28 @@ chmod +x $CORE_HOME/bin/*
 
 cored init $MONIKER $CORE_CHAIN_ID_ARGS
 
-cored start --chain-id=coreum-testnet-1
+echo -e "\e[1m\e[32m4. Starting service... \e[0m" && sleep 1
+# create service
+sudo tee /etc/systemd/system/cored.service > /dev/null <<EOF
+[Unit]
+Description=Cored node
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which cored) start --home $HOME/.core/
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload && \
+sudo systemctl enable cored && \
+sudo systemctl restart cored && \
+sudo journalctl -u cored -f -o cat
+
+
 
