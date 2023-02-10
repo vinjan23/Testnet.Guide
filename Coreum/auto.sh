3391ba330d8ef7aab74ad7f34b5ec1136bdf2b63
vinjan <108977419+vinjan23@users.noreply.github.com>
2023-02-10 20:18:56 +0700

    Update auto.sh

--- Coreum/auto.sh
+++ Coreum/auto.sh
@@ -12,16 +12,13 @@ sleep 2
 
 read -p "Enter node moniker:"MONIKER
 
-export MONIKER
-export CORE_CHAIN_ID="coreum-testnet-1"
-export CORE_DENOM="utestcore"
-export CORE_NODE="https://full-node-pluto.testnet-1.coreum.dev"
-export CORE_FAUCET_URL="https://api.testnet-1.coreum.dev"
-export CORE_VERSION="v0.1.1"
-export CORE_CHAIN_ID_ARGS="--chain-id=$CORE_CHAIN_ID"
-export CORE_NODE_ARGS="--node=$CORE_NODE $CORE_CHAIN_ID_ARGS"
-export CORE_HOME=$HOME/.core/"$CORE_CHAIN_ID"
-export CORE_BINARY_NAME=$(arch | sed s/aarch64/cored-linux-arm64/ | sed s/x86_64/cored-linux-amd64/)
+CHAIN_ID="coreum-testnet-1"
+CHAIN_DENOM="utestcore"
+BINARY="cored"
+
+echo -e "Node moniker: ${CYAN}$NODE_MONIKER${NC}"
+echo -e "Chain id:     ${CYAN}$CHAIN_ID${NC}"
+echo -e "Chain demon:  ${CYAN}$CHAIN_DENOM${NC}"
 sleep 2
 
 echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
@@ -47,10 +44,37 @@ fi
 
 echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
 # download and build binaries
-mkdir -p $CORE_HOME/bin
-curl -LO https://github.com/CoreumFoundation/coreum/releases/download/$CORE_VERSION/$CORE_BINARY_NAME
-mv $CORE_BINARY_NAME $CORE_HOME/bin/cored
-chmod +x $CORE_HOME/bin/*
+cd $HOME
+curl -LOf https://github.com/CoreumFoundation/coreum/releases/download/v0.1.1/cored-linux-amd64
+chmod +x cored-linux-amd64
+mv cored-linux-amd64 cored
+mv cored $HOME/go/bin/
+
+# init
+cored config keyring-backend test
+cored config node tcp://localhost:${PORT}657
+cored init $MONIKER --chain-id $CHAIN_ID
+
+# Set peers and seeds
+SEEDS=
+sed -i -e "s|^seeds *=.*|seeds = \"$SEEDS\"|" $HOME/$FOLDER/$CHAIN/config/config.toml
+
+# Download genesis and addrbook
+#curl -Ls $GENESIS > $HOME/$FOLDER/$CHAIN/config/genesis.json
+#curl -Ls $ADDRBOOK > $HOME/$FOLDER/$CHAIN/config/addrbook.json
+
+# Set Config Pruning
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="10"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/$FOLDER/$CHAIN/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/$FOLDER/$CHAIN/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/$FOLDER/$CHAIN/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/$FOLDER/$CHAIN/config/app.toml
+
+# Set minimum gas price
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0$DENOM\"/" $HOME/$FOLDER/$CHAIN/config/app.toml
 
 echo -e "\e[1m\e[32m4. Starting service... \e[0m" && sleep 1
 # create service
