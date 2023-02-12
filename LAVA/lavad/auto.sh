98e9879ebc87b85315677fc04a3af44a27455656
vinjan <108977419+vinjan23@users.noreply.github.com>
2023-02-12 12:23:32 +0700

    Update auto.sh

--- LAVA/lavad/auto.sh
+++ LAVA/lavad/auto.sh
@@ -12,14 +12,14 @@ echo -e "\e[0m"
 
 sleep 2
 
-read -p "Enter node moniker: " NODE_MONIKER
+read -p "Enter node moniker: " MONIKER
 
 CHAIN_ID="lava-testnet-1"
 CHAIN_DENOM="ulava"
 BINARY="lavad"
 
 
-echo -e "Node moniker: ${CYAN}$NODE_MONIKER${NC}"
+echo -e "Moniker: ${CYAN}$MONIKER${NC}"
 echo -e "Chain id:     ${CYAN}$CHAIN_ID${NC}"
 echo -e "Chain demon:  ${CYAN}$CHAIN_DENOM${NC}"
 sleep 2
@@ -46,19 +46,50 @@ fi
 
 echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
 # download and build binaries
-git clone https://github.com/K433QLtr6RA9ExEq/GHFkqmTzpdNLDd6T.git
-mkdir -p $HOME/go/bin
-mkdir -p $HOME/.lava/config
-wget -qO $HOME/go/bin/lavad https://lava-binary-upgrades.s3.amazonaws.com/testnet/v0.4.0/lavad
-chmod +x $HOME/go/bin/lavad
-cp GHFkqmTzpdNLDd6T/testnet-1/genesis_json/genesis.json $HOME/.lava/config/genesis.json
-cp GHFkqmTzpdNLDd6T/testnet-1/default_lavad_config_files/* $HOME/.lava/config
-wget -qO $HOME/.lava/config/addrbook.json http://94.250.203.6:90/lava-addrbook.json
+cd $HOME
+rm -rf $HOME/lava
+git clone https://github.com/lavanet/lava.git
+cd lava
+git checkout v0.5.2
+make install
 
 #config
+PORT=17
+lavad init $MONIKER --chain-id lava-testnet-1
 lavad config chain-id lava-testnet-1
-lavad config keyring-backend file
-lavad config node tcp://localhost:26657
+lavad config keyring-backend test
+lavad config node tcp://localhost:${PORT}657
+
+curl https://raw.githubusercontent.com/K433QLtr6RA9ExEq/GHFkqmTzpdNLDd6T/main/testnet-1/genesis_json/genesis.json > ~/.lava/config/genesis.json
+wget -O $HOME/.lava/config/addrbook.json "https://raw.githubusercontent.com/vinjan23/Testnet.Guide/main/LAVA/Manual/addrbook.json"
+
+# seed & peer
+SEEDS="3a445bfdbe2d0c8ee82461633aa3af31bc2b4dc0@prod-pnet-seed-node.lavanet.xyz:26656,e593c7a9ca61f5616119d6beb5bd8ef5dd28d62d@prod-pnet-seed-node2.lavanet.xyz:26656"
+PEERS="49b998f8c91005cbc1050380a9bd293be56ba759@95.217.41.58:26656,4b1dfa6c538de8d13a116bc68205636e42d6fbbd@146.190.82.119:26656,bd1e1f8df77e7b61200c490c9fabe6bbc4412d4e@91.223.3.144:26856,d3eb474a1f90d004e49638e384069c32d7dcc8a2@185.252.232.110:26656"
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.lava/config/config.toml
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0ulava\"/" $HOME/.lava/config/app.toml
+sed -i 's/create_empty_blocks = .*/create_empty_blocks = true/g' ~/.lava/config/config.toml
+sed -i 's/create_empty_blocks_interval = ".*s"/create_empty_blocks_interval = "60s"/g' ~/.lava/config/config.toml
+sed -i 's/timeout_propose = ".*s"/timeout_propose = "60s"/g' ~/.lava/config/config.toml
+sed -i 's/timeout_commit = ".*s"/timeout_commit = "60s"/g' ~/.lava/config/config.toml
+sed -i 's/timeout_broadcast_tx_commit = ".*s"/timeout_broadcast_tx_commit = "601s"/g' ~/.lava/config/config.toml
+sed -i 's/max_num_inbound_peers =.*/max_num_inbound_peers = 50/g' $HOME/.lava/config/config.toml
+sed -i 's/max_num_outbound_peers =.*/max_num_outbound_peers = 50/g' $HOME/.lava/config/config.toml
+
+# prunning
+pruning="custom" && \
+pruning_keep_recent="100" && \
+pruning_keep_every="0" && \
+pruning_interval="10" && \
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" ~/.lava/config/app.toml && \
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" ~/.lava/config/app.toml && \
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" ~/.lava/config/app.toml && \
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" ~/.lava/config/app.toml
+
+# custom port
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://0.0.0.0:${PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${PORT}660\"%" $HOME/.lava/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${PORT}317\"%; s%^address = \":8080\"%address = \":${PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${PORT}091\"%; s%^address = \"0.0.0.0:8545\"%address = \"0.0.0.0:${PORT}545\"%; s%^ws-address = \"0.0.0.0:8546\"%ws-address = \"0.0.0.0:${PORT}546\"%" $HOME/.lava/config/app.toml
+sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.lava/config/config.toml
 
 
 echo -e "\e[1m\e[32m4. Starting service... \e[0m" && sleep 1
@@ -67,13 +98,14 @@ sudo tee /etc/systemd/system/lavad.service << EOF
 [Unit]
 Description=Lava Node
 After=network-online.target
+
 [Service]
 User=$USER
 ExecStart=$(which lavad) start --home="$HOME/.lava"
-Restart=always
-RestartSec=30
-LimitNOFILE=infinity
-LimitNPROC=infinity
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -81,13 +113,7 @@ EOF
 # start service
 sudo systemctl daemon-reload
 sudo systemctl enable lavad
-snap=$(curl -s http://94.250.203.6:90 | egrep -o ">lavad-snap*.*tar" | tr -d ">")
-rm -rf  $HOME/.lava/data
-wget -P $HOME http://94.250.203.6:90/${snap}
-tar xf $HOME/${snap} -C $HOME/.lava/
-rm $HOME/${snap}
-sudo systemctl restart lavad
-sudo journalctl -u lavad -f -o cat
+sudo systemctl restart lavad && sudo journalctl -u lavad -f -o cat
 
 echo '=============== SETUP FINISHED ==================='
 echo -e 'To check logs: \e[1m\e[32mjournalctl -u lavad -f -o cat\e[0m'
