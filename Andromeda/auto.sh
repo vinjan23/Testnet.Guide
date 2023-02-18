bcd7de7af8eb0fccc0bf39c97623810e6d712259
vinjan <108977419+vinjan23@users.noreply.github.com>
2023-02-18 14:59:22 +0700

    Create auto.sh

---
+++ Andromeda/auto.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+echo -e "\033[0;35m"
+echo "    #         #   #  #  #     #          #      #  #      #  #     #    ";
+echo "     #       #    #  #   #    #          #     #    #     #   #    #    ";
+echo "      #     #     #  #     #  #          #    #  # # #    #     #  #    ";
+echo "       #   #      #  #      # #          #   #        #   #      # #    ";
+echo "         #        #  #        #   # # # #   #          #  #        #    ";
+echo -e "\e[0m"
+sleep 2
+
+read -p "Enter node moniker:" NODE_MONIKER
+
+CHAIN_ID="galileo-3 -y"
+CHAIN_DENOM="andr"
+BINARY="andromedad"
+PORT="23"
+
+echo -e "Node moniker: ${CYAN}$NODE_MONIKER${NC}"
+echo -e "Chain id:     ${CYAN}$CHAIN_ID${NC}"
+echo -e "Chain demon:  ${CYAN}$CHAIN_DENOM${NC}"
+echo -e "PORT:         ${CYAN}$PORT{NC}"       
+sleep 2
+
+echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
+# update
+sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
+# packages
+sudo apt install curl tar wget clang pkg-config libssl-dev libleveldb-dev jq build-essential bsdmainutils git make ncdu htop screen unzip bc fail2ban htop -y
+
+# install go
+sudo rm -rf /usr/local/go
+curl -Ls https://go.dev/dl/go1.19.5.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
+eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
+
+echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
+# download and build binaries
+cd ~
+git clone https://github.com/andromedaprotocol/andromedad
+cd andromedad
+git checkout galileo-3-v1.1.0-beta1
+make install
+
+# init
+andromedad config keyring-backend test
+andromedad config chain-id $CHAIN_ID
+andromedad init "$NODE_MONIKER" --chain-id $CHAIN_ID
+andromedadconfig node tcp://localhost:${PORT}657
+
+# download genesis
+curl -o $HOME/.andromedad/config/genesis.json https://raw.githubusercontent.com/andromedaprotocol/testnets/galileo-3/genesis.json
+wget -O $HOME/.andromedad/config/addrbook.json "https://raw.githubusercontent.com/nodersteam/cosmos-adrbook/main/andromeda/addrbook.json"
+
+# Set peers and seeds
+sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.0andr\"/;" ~/.andromedad/config/app.toml
+sed -i -e "s/^filter_peers *=.*/filter_peers = \"true\"/" $HOME/.andromedad/config/config.toml
+external_address=$(wget -qO- eth0.me)
+sed -i.bak -e "s/^external_address *=.*/external_address = \"$external_address:26656\"/" $HOME/.andromedad/config/config.toml
+peers=""
+sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" $HOME/.andromedad/config/config.toml
+seeds=""
+sed -i.bak -e "s/^seeds =.*/seeds = \"$seeds\"/" $HOME/.andromedad/config/config.toml
+sed -i 's/max_num_inbound_peers =.*/max_num_inbound_peers = 100/g' $HOME/.andromedad/config/config.toml
+sed -i 's/max_num_outbound_peers =.*/max_num_outbound_peers = 100/g' $HOME/.andromedad/config/config.toml
+
+# Set Config Pruning
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="50"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.andromedad/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.andromedad/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.andromedad/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.andromedad/config/app.toml
+
+sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.andromedad/config/config.toml
+
+# custom port
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://0.0.0.0:${PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${PORT}660\"%" $HOME/.andromedad/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${PORT}317\"%; s%^address = \":8080\"%address = \":${PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${PORT}091\"%; s%^address = \"0.0.0.0:8545\"%address = \"0.0.0.0:${PORT}545\"%; s%^ws-address = \"0.0.0.0:8546\"%ws-address = \"0.0.0.0:${PORT}546\"%" $HOME/.andromedad/config/app.toml
+
+# statesync
+peers="99cebda3a65a35b9a6a8bef774c8b92c1e548aa5@andromeda.rpc.nodersteam.com:36656"
+sed -i.bak -e  "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" ~/.andromedad/config/config.toml
+
+SNAP_RPC=http://andromeda.rpc.nodersteam.com:36657
+
+LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 500)); \
+TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+
+echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
+
+sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
+s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
+s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"\"|" $HOME/.andromedad/config/config.toml
+echo -e "\e[1m\e[32m4. Starting service... \e[0m" && sleep 1
+# create service
+sudo tee /etc/systemd/system/andromedad.service > /dev/null <<EOF
+[Unit]
+Description=andromedad node
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which andromedad) start
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload && \
+sudo systemctl enable andromedad && \
+sudo systemctl restart andromedad && \
+sudo journalctl -u andromedad -f -o cat
+
+
+
+
