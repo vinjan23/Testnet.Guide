2c717fba2f5212238f90b9bdee1544e6fd41fae3
vinjan <108977419+vinjan23@users.noreply.github.com>
2023-05-22 10:54:40 +0700

    Create statesync.sh

---
+++ C4E/statesync.sh
@@ -0,0 +1,14 @@
+RPC="http://66.70.177.76:36657"
+LATEST_HEIGHT=$(curl -s $RPC/block | jq -r .result.block.header.height); \
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 500)); \
+TRUST_HASH=$(curl -s "$RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+
+echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
+
+sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$RPC,$RPC\"| ; \
+s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
+s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"\"|" $HOME/.c4e-chain/config/config.toml
+c4ed tendermint unsafe-reset-all --home /root/.c4e-chain
+sudo systemctl restart c4ed && journalctl -u c4ed -f -o cat
